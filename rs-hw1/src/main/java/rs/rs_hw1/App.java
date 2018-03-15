6dbe9179ad68d8798dfb8bc8ea34db90deb937c3
Hrizantema Stancheva <hrizantema.st@gmail.com>
2018-03-15 17:05:10 +0200

    Add files via upload

--- rs-hw1/src/main/java/rs/rs_hw1/App.java
+++ rs-hw1/src/main/java/rs/rs_hw1/App.java
@@ -1,40 +1,32 @@
-package rs.rs_hw1;
+package rs.hw1;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.io.Reader;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queries.mlt.MoreLikeThis;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
 
 public class App {
-	private static void addDocuments(IndexWriter w, File[] fileNames) throws IOException {
-		for (File file : fileNames) {
-			{
-				try {
-					String fileContent = readContent(file);
-					System.out.println("filecontent size: " + fileContent.length());
-					String fileName = file.getName();
-					System.out.println("filecontent name: " + fileName);
-					System.out.println("=======================");
-					addDoc(w, fileName, fileContent);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+	
+
 	public static String readTXTFile(final String csvFileName) throws IOException {
 
 		String line = null;
@@ -64,34 +56,51 @@ public class App {
 
 	@SuppressWarnings("nls")
 	public static void main(String[] args) throws IOException {
-		/*StandardAnalyzer analyzer = new StandardAnalyzer();
+		//create
+		StandardAnalyzer analyzer = new StandardAnalyzer();
 		Directory index = new RAMDirectory();
-
 		IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
-		IndexWriter w = new IndexWriter(index, config);*/
-		/*
-		 * File[] fileNames = listAllFiles(new
-		 * File("..\\resources\\20_newsgroups\\alt.atheism"));
-		 * 
-		 * System.out.println("filenames size: " + fileNames.length);
-		 * addDocuments(w, fileNames);
-		 */
-		File f = new File("resources\\20_newsgroups\\alt.atheism\\49960.txt");
-		System.out.println(f.getAbsolutePath());
-		System.out.println(f.isDirectory());
-		//w.close();
-		String fileName = "resources\\49960.txt";
-		List<String> allLinesFromDataset = new ArrayList<>();
-
-		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-
-			allLinesFromDataset = stream.collect(Collectors.toList());
-
+		IndexWriter w = null;
+		try {
+			w = new IndexWriter(index, config);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println(allLinesFromDataset.size());
+
+		File[] fileNames = listAllFiles(new File("src\\main\\resources\\user1"));
+		System.out.println("filenames size: " + fileNames.length);
+		addDocuments(w, fileNames);
+		//close writer to commit changes
+		w.close();
+		
+		//search
+		IndexReader reader = DirectoryReader.open(index);
+		IndexSearcher searcher = new IndexSearcher(reader);
+		
+		// comparison
+		MoreLikeThis mlt = new MoreLikeThis(reader); // Pass the index reader
+		
+		mlt.setAnalyzer(analyzer); // "main" java.lang.UnsupportedOperationException: To use MoreLikeThis without term vectors, you must provide an Analyzer
+		
+		
+		mlt.setFieldNames(new String[] {"content"}); // specify the fields for similiarity
+	 	Reader target = new BufferedReader(new FileReader("src\\main\\resources\\user2\\51121")); // orig source of doc you want to find similarities to
+		Query query = mlt.like("content", target); // Pass the doc id 
+		TopDocs similarDocs = searcher.search(query, 1); // Use the searcher
+		ScoreDoc[] hits = similarDocs.scoreDocs;
+		if (similarDocs.totalHits == 0)
+		    // Do handling
+			System.out.println();
+		System.out.println("scores: " + similarDocs.getMaxScore());
+		//display
+		System.out.println("Found " + hits.length + " hits.");
+		for(int i=0;i<hits.length;++i) {
+		    int docId = hits[i].doc;
+		    Document d = searcher.doc(docId);
+		    System.out.println((i + 1) + ". " + d.get("content") + "\t" + d.get("title"));
+		}
+		
+
 	}
 
 	private static void addDoc(final IndexWriter w, final String title, final String content) throws IOException {
@@ -99,5 +108,22 @@ public class App {
 		System.out.println("addDoc called");
 		doc.add(new TextField("title", title, Field.Store.YES));
 		doc.add(new StringField("content", content, Field.Store.YES));
+	}
+	
+	private static void addDocuments(IndexWriter w, File[] fileNames) throws IOException {
+		for (File file : fileNames) {
+			{
+				try {
+					String fileContent = readContent(file);
+					System.out.println("filecontent size: " + fileContent.length());
+					String fileName = file.getName();
+					System.out.println("filecontent name: " + fileName);
+					System.out.println("=======================");
+					addDoc(w, fileName, fileContent);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
--- rs-hw1/src/main/java/rs/rs_hw1/User.java
+++ rs-hw1/src/main/java/rs/rs_hw1/User.java
@@ -1,5 +1,37 @@
-package rs.rs_hw1;
+package rs.hw1;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
 
 public class User {
 
+	public static void main(String... args) {
+		StandardAnalyzer analyzer = new StandardAnalyzer();
+		Directory index = new RAMDirectory();
+		IndexWriterConfig config = new IndexWriterConfig(analyzer);
+		try {
+			IndexWriter w = new IndexWriter(index, config);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void showFiles(File[] files) {
+	    for (File file : files) {
+	        if (file.isDirectory()) {
+	            System.out.println("Directory: " + file.getName());
+	            showFiles(file.listFiles()); // Calls same method again.
+	        } else {
+	            System.out.println("File: " + file.getName());
+	        }
+	    }
+	}
 }
